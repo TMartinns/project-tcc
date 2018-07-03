d188ec8e3e875fc1f19b6507271e0fcd86c6ff83
Thales Martins <thales.martins35@gmail.com>
2018-07-03 20:02:15 -0300

    Foi definido uma constante para o caminho "public/js".

--- interface.php
+++ interface.php
@@ -5,6 +5,7 @@ define('BASE', '');
 define('BOWER', 'public/bower_components/');
 define('CSS', 'public/css/');
 define('IMG', 'public/img/');
+define('JS', 'public/js/');
 
 $view_title = 'ADUV';
 $add_js = $add_css = '';
