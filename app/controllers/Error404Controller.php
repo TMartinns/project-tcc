704af9ea5a71309cc1bdac315be83f5738413a68
Thales Martins <thales.martins35@gmail.com>
2018-07-03 20:06:34 -0300

    Foi adicionado um layout personalizado para a view error404.

--- app/controllers/Error404Controller.php
+++ app/controllers/Error404Controller.php
@@ -6,6 +6,7 @@ class Error404Controller extends \HXPHP\System\Controller
 {
     public function indexAction()
     {
-        $this->view->setAssets('css', $this->configs->baseURI . 'public/css/error.css');
+        $this->view->setHeader('error404/header');
+        $this->view->setFooter('error404/footer');
     }
 }
