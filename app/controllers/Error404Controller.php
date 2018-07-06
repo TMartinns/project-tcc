028eef41fb4b8cef0bd2827de4ce7ebc46039926
Thales Martins <thales.martins35@gmail.com>
2018-07-06 16:11:37 -0300

    Modificado a view index do controller error404, para não necessitar de header e footer separadamente, já que nenhuma outra view irá aproveitar de seu header e footer.

--- app/controllers/Error404Controller.php
+++ app/controllers/Error404Controller.php
@@ -6,7 +6,6 @@ class Error404Controller extends \HXPHP\System\Controller
 {
     public function indexAction()
     {
-        $this->view->setHeader('error404/header');
-        $this->view->setFooter('error404/footer');
+        $this->view->setTemplate(false);
     }
 }
