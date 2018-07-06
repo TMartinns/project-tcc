0acb5187deb8c35900b5eb4fdc5d22f440aa2344
Thales Martins <thales.martins35@gmail.com>
2018-07-06 17:18:42 -0300

    Modificado o "find" utilizado na action acessar de "find_by_sql" para "find_by_nome_usuario".

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -30,7 +30,7 @@ class IndexController extends \HXPHP\System\Controller
 
         $post = $this->request->post();
         $nome_usuario = $post['nome_usuario'];
-        $usuario = Usuario::find_by_sql("select * from usuarios where nome_usuario = '$nome_usuario'")[0];
+        $usuario = Usuario::find_by_nome_usuario($nome_usuario);
 
         if (password_verify($post['senha'], $usuario->senha)) {
             $this->auth->login($usuario->id_pessoa, $usuario->nome_usuario, $usuario->permissao);
