8c52eac073e2e581b71dd3d9092c49a15f0eb26a
Thales Martins <thales.martins35@gmail.com>
2018-07-06 17:42:09 -0300

    Configurado todas as views do controller usuarios como privadas, e incluídos arquivos css e js no header e footer padrão da aplicação, sempre que a view index do controller usuarios for chamada.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -2,5 +2,29 @@
 
 class UsuariosController extends \HXPHP\System\Controller
 {
+    public function __construct(\HXPHP\System\Configs\Config $configs = null)
+    {
+        parent::__construct($configs);
 
+        $this->load(
+            'Services\Auth',
+            $configs->auth->after_login,
+            $configs->auth->after_logout,
+            true
+        );
+
+        $this->auth->redirectCheck(false);
+    }
+
+    public function indexAction()
+    {
+        $this->view->setAssets('css',
+            $this->configs->baseURI . 'public/bower_components/gijgo/dist/combined/css/gijgo.min.css');
+        $this->view->setAssets('js',
+            $this->configs->baseURI . 'public/bower_components/gijgo/dist/combined/js/gijgo.min.js');
+        $this->view->setAssets('js',
+            $this->configs->baseURI . 'public/bower_components/gijgo/dist/combined/js/messages/messages.pt-br.min.js');
+        $this->view->setAssets('js',
+            $this->configs->baseURI . 'public/js/datepicker.config.js');
+    }
 }
