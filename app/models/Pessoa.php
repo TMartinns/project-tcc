e80a0f26c58fc1daad4f6b65f6881fb35f3437fe
Thales Martins <thales.martins35@gmail.com>
2018-07-06 20:48:51 -0300

    Adicionado o método cadastrar e configurado as validações que deverão ser feitas nas models pessoa e usuario.

--- app/models/Pessoa.php
+++ app/models/Pessoa.php
@@ -3,4 +3,76 @@
 class Pessoa extends \HXPHP\System\Model
 {
     static $table_name = 'pessoas';
+
+    static $validates_presence_of = array(
+        array(
+            'nome',
+            'message' => 'O nome é um campo obrigatório.'
+        ),
+        array(
+            'cpf',
+            'message' => 'O CPF é um campo obrigatório.'
+        )
+    );
+
+    static $validates_uniqueness_of = array(
+        array(
+            'cpf',
+            'message' => 'Já existe um usuário com esse CPF cadastrado.'
+        )
+    );
+
+    public static function cadastrar(array $post, $completo = false)
+    {
+        $callback = new \stdClass;
+        $callback->pessoa = null;
+        $callback->status = false;
+        $callback->errors = array();
+
+        if($completo == false) {
+            $pessoa = self::create($post);
+
+            if($pessoa->is_valid()) {
+                $callback->pessoa = $pessoa;
+                $callback->status = true;
+                return $callback;
+            }
+
+            $errors = $pessoa->errors->get_raw_errors();
+
+            foreach ($errors as $field => $message) {
+                array_push($callback->errors, $message[0]);
+            }
+
+            return $callback;
+        }
+
+
+        self::$validates_presence_of = array_push(self::$validates_presence_of,
+            array(
+                'data_nascimento',
+                'message' => 'A data de nascimento é um campo obrigatório.'
+            ),
+            array(
+                'nome_mae',
+                'message' => 'O nome da mãe é um campo obrigatório.'
+            )
+        );
+
+        $pessoa = self::create($post);
+
+        if($pessoa->is_valid()) {
+            $callback->pessoa = $pessoa;
+            $callback->status = 1;
+            return $callback;
+        }
+
+        $errors = $pessoa->errors->get_raw_errors();
+
+        foreach ($errors as $field => $message) {
+            array_push($callback->errors, $message[0]);
+        }
+
+        return $callback;
+    }
 }
--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -4,4 +4,53 @@ class Usuario extends \HXPHP\System\Model
 {
     static $table_name = 'usuarios';
 
+    static $validates_presence_of = array(
+        array(
+            'nome_usuario',
+            'message' => 'O nome de usuário é um campo obrigatório.'
+        ),
+        array(
+            'email',
+            'message' => 'O e-mail é um campo obrigatório.'
+        ),
+        array(
+            'permissao',
+            'message' => 'A permissão é um campo obrigatório.'
+        )
+    );
+
+    static $validates_uniqueness_of = array(
+        array(
+            'nome_usuario',
+            'message' => 'Já existe um usuário com esse nome de usuário cadastrado.'
+        ),
+        array(
+            'email',
+            'message' => 'Já existe um usuário com esse e-mail cadastrado.'
+        )
+    );
+
+    public function cadastrar(array $post)
+    {
+        $callback = new \stdClass;
+        $callback->usuario = null;
+        $callback->status = false;
+        $callback->errors = array();
+
+        $usuario = self::create($post);
+
+        if($usuario->is_valid()) {
+            $callback->usuario = $usuario;
+            $callback->status = true;
+            return $callback;
+        }
+
+        $errors = $usuario->errors->get_raw_errors();
+
+        foreach ($errors as $field => $message) {
+            array_push($callback->errors, $message[0]);
+        }
+
+        return $callback;
+    }
 }
