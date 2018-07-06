09d26215d413f987e60d5d2a50d862bc5c90e228
Thales Martins <thales.martins35@gmail.com>
2018-07-06 17:09:16 -0300

    Configurado a view home como página privada, garantindo o acesso a mesma somente ao pessoal autorizado.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,5 +2,18 @@
 
 class HomeController extends \HXPHP\System\Controller
 {
+    public function __construct(\HXPHP\System\Configs\Config $configs = null)
+    {
+        parent::__construct($configs);
+
+        $this->load(
+            'Services\Auth',
+            $configs->auth->after_login,
+            $configs->auth->after_logout,
+            true
+        );
+
+        $this->auth->redirectCheck(false);
+    }
 
 }
