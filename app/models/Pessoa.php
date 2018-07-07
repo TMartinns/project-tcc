490ebbbdc0622cc87a8a589afb09bc2f1fb393bd
Thales Martins <thales.martins35@gmail.com>
2018-07-07 03:58:28 -0300

    Modificado o escopo do método cadastrar do model pessoa, a fim de retirar uma condição que nunca será utilizada.

--- app/models/Pessoa.php
+++ app/models/Pessoa.php
@@ -22,48 +22,18 @@ class Pessoa extends \HXPHP\System\Model
         )
     );
 
-    public static function cadastrar(array $post, $completo = false)
+    public static function cadastrar(array $post)
     {
         $callback = new \stdClass;
         $callback->pessoa = null;
         $callback->status = false;
         $callback->errors = array();
 
-        if($completo == false) {
-            $pessoa = self::create($post);
-
-            if($pessoa->is_valid()) {
-                $callback->pessoa = $pessoa;
-                $callback->status = true;
-                return $callback;
-            }
-
-            $errors = $pessoa->errors->get_raw_errors();
-
-            foreach ($errors as $field => $message) {
-                array_push($callback->errors, $message[0]);
-            }
-
-            return $callback;
-        }
-
-
-        self::$validates_presence_of = array_push(self::$validates_presence_of,
-            array(
-                'data_nascimento',
-                'message' => 'A data de nascimento é um campo obrigatório.'
-            ),
-            array(
-                'nome_mae',
-                'message' => 'O nome da mãe é um campo obrigatório.'
-            )
-        );
-
         $pessoa = self::create($post);
 
-        if($pessoa->is_valid()) {
+        if ($pessoa->is_valid()) {
             $callback->pessoa = $pessoa;
-            $callback->status = 1;
+            $callback->status = true;
             return $callback;
         }
 
