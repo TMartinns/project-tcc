b946eb38c556a81bc95620c932a02c694d1956f0
Thales Martins <thales.martins35@gmail.com>
2018-07-07 01:48:07 -0300

    Concluído a action cadastrar do controller usuarios.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -16,15 +16,58 @@ class UsuariosController extends \HXPHP\System\Controller
         $this->auth->redirectCheck(false);
     }
 
-    public function indexAction()
+    public function cadastrarAction()
     {
-        $this->view->setAssets('css',
-            $this->configs->bower . 'gijgo/dist/combined/css/gijgo.min.css');
-        $this->view->setAssets('js',
-            $this->configs->bower . 'gijgo/dist/combined/js/gijgo.min.js');
-        $this->view->setAssets('js',
-            $this->configs->bower . 'gijgo/dist/combined/js/messages/messages.pt-br.min.js');
-        $this->view->setAssets('js',
-            $this->configs->js . 'datepicker.config.js');
+        $this->view->setFile('index');
+
+        $this->request->setCustomFilters(array(
+            'email' => FILTER_VALIDATE_EMAIL
+        ));
+
+        $post = $this->request->post();
+
+        if(!empty($post)) {
+            $pessoa = array(
+                'nome' => $post['nome'],
+                'cpf' => $post['cpf']
+            );
+
+            $callback = Pessoa::cadastrar($pessoa);
+
+            if($callback->status == true) {
+                $senha = $this->configs->random->password();
+                $usuario = array(
+                    'nome_usuario' => $post['nome_usuario'],
+                    'senha' => password_hash($senha, PASSWORD_DEFAULT),
+                    'email' => $post['email'],
+                    'permissao' => $post['permissao'],
+                    'id_pessoa' => $callback->pessoa->id
+                );
+
+                $callback = Usuario::cadastrar($usuario);
+
+                if($callback->status == true) {
+                    $this->load('Helpers\Alert', array(
+                       'success',
+                       'O cadastro foi completado com sucesso!'
+                    ));
+
+                } else {
+                    $pessoa = Pessoa::find_by_id($usuario['id_pessoa']);
+                    $pessoa->delete();
+                    $this->load('Helpers\Alert', array(
+                        'danger',
+                        'Não foi possível completar o cadastro em razão dos seguintes erros:',
+                        $callback->errors
+                    ));
+                }
+            } else {
+                $this->load('Helpers\Alert', array(
+                    'danger',
+                    'Não foi possível completar o cadastro em razão dos seguintes erros:',
+                    $callback->errors
+                ));
+            }
+        }
     }
 }
