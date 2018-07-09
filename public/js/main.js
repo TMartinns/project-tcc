5f9272246f6b14b0cd898c594677a815f4bd0bf1
Thales Martins <thales.martins35@gmail.com>
2018-07-09 18:06:40 -0300

    Movido o conteúdo do arquivo "sidebar.css" para o arquivo "main.css" e adicionada novas interações na "main.js".

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,19 @@ $(document).ready(function () {
         $('#container').toggleClass('sidebar-active');
     });
 
-    $('#link-pesquisar').on('click', function () {
-        $('#input-pesquisar').toggleClass('d-none');
-    });
+    $('#sidebar a.active').toggleClass('text-primary');
+
+    if(window.matchMedia("(max-width: 425px)").matches) {
+        $('div#input-pesquisar').toggleClass('d-none');
+        $('#menu').prepend("<li class=\"nav-item\">\n" +
+            "                    <div id=\"input-pesquisar\" class=\"input-group\">\n" +
+            "                        <input class=\"form-control\" type=\"search\" placeholder=\"Pesquisar\" aria-label=\"\">\n" +
+            "                        <div class=\"input-group-append\">\n" +
+            "                            <button type=\"button\" class=\"btn btn-outline-dark\">\n" +
+            "                                <i class=\"fas fa-search\"></i>\n" +
+            "                            </button>\n" +
+            "                        </div>\n" +
+            "                    </div>\n" +
+            "                </li>");
+    }
 });
