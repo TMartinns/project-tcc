2e1bf31ea0086469b4d3e5abe6f786932610d6ec
Thales Martins <thales.martins35@gmail.com>
2018-07-10 20:41:07 -0300

    Implementado a API reCaptcha para evitar ataques de força bruta.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,6 +12,15 @@ class IndexController extends \HXPHP\System\Controller
             $configs->auth->after_logout,
             true
         );
+
+        $this->load('Storage\Session');
+
+        $ipBloqueados = $this->session->get('ipBloqueados');
+        if(!is_null($ipBloqueados)) {
+            if (in_array($this->request->server('REMOTE_ADDR'), $ipBloqueados, true)) {
+                $this->view->setPath('blank');
+            }
+        }
     }
 
     public function indexAction()
@@ -31,25 +40,38 @@ class IndexController extends \HXPHP\System\Controller
         $post = $this->request->post();
 
         if (!empty($post)) {
-            $nome_usuario = $post['nome_usuario'];
-            $usuario = Usuario::find_by_nome_usuario($nome_usuario);
+            if($this->captchaSuccess($post['g-recaptcha-response'])) {
+                $usuario = Usuario::find_by_nome_usuario($post['nome_usuario']);
 
-            if (!empty($usuario)) {
-                if (password_verify($post['senha'], $usuario->senha)) {
-                    $this->auth->login($usuario->id_pessoa, $usuario->nome_usuario, $usuario->funcao);
+                if (!empty($usuario)) {
+                    if (password_verify($post['senha'], $usuario->senha)) {
+                        $this->auth->login($usuario->id_pessoa, $usuario->nome_usuario, $usuario->funcao);
+                    } else {
+                        $this->load('Modules\Messages', 'auth');
+                        $this->messages->setBlock('alerts');
+                        $this->load('Helpers\Alert',
+                            $this->messages->getByCode('dados-incorretos')
+                        );
+                    }
                 } else {
                     $this->load('Modules\Messages', 'auth');
                     $this->messages->setBlock('alerts');
                     $this->load('Helpers\Alert',
-                        $this->messages->getByCode('dados-incorretos')
+                        $this->messages->getByCode('usuario-inexistente')
                     );
                 }
             } else {
-                $this->load('Modules\Messages', 'auth');
-                $this->messages->setBlock('alerts');
-                $this->load('Helpers\Alert',
-                    $this->messages->getByCode('usuario-inexistente')
-                );
+                $this->load('Storage\Session');
+
+                $ipBloqueados = array($this->request->server('REMOTE_ADDR'));
+
+                if($this->session->exists('ipBloqueados')) {
+                    $ipBloqueados = array_merge($ipBloqueados, $this->session->get('ipBloqueados'));
+                }
+
+                $this->session->set('ipBloqueados', $ipBloqueados);
+
+                $this->view->setPath('blank');
             }
         }
     }
@@ -57,5 +79,16 @@ class IndexController extends \HXPHP\System\Controller
     public function sairAction()
     {
         $this->auth->logout();
+    }
+
+    private function captchaSuccess($response)
+    {
+        return json_decode(\HXPHP\System\Services\SimplecURL\SimplecURL::connect(
+            'https://www.google.com/recaptcha/api/siteverify',
+            array(
+                'secret' => '6Ldqc2MUAAAAAK-HjGqf-nj0DC99adUy-Ry1SvNJ',
+                'response' => $response
+            )
+        ), true)['success'];
     }
 }
