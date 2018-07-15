38cb21c3de797503656f1e510ee89781f9aae12c
Thales Martins <thales.martins35@gmail.com>
2018-07-15 18:18:21 -0300

    Modificado a cor dos itens da sidebar, utilizando a classe "text-dark" do bootstrap 4.

--- app/config.php
+++ app/config.php
@@ -31,7 +31,7 @@ $configs->env->development->menu->setConfigs(array(
     'menu_id' => 'sidebarItens',
     'menu_item_class' => 'nav-item',
     'link_before' => '<span class="sidebarItem">',
-    'link_class' => 'nav-link',
+    'link_class' => 'nav-link text-dark',
     'link_active_class' => 'active border-top border-bottom border-primary'
 ));
 
