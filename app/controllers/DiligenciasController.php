2fd49855defe1a95b0b62a238a37556ff7107f83
Thales Martins <thales.martins35@gmail.com>
2018-08-02 16:03:37 -0300

    Foi criado o método cadastrar no controller "Diligencias" para efetuar o registro de uma diligência, "chamando" métodos para persistência dos dados e fornecendo um "callback" ao usuário através de "alerts" na view.

--- app/controllers/DiligenciasController.php
+++ app/controllers/DiligenciasController.php
@@ -33,4 +33,66 @@ class DiligenciasController extends \HXPHP\System\Controller
             $this->configs->js . 'datepicker.config.js'
         ));
     }
+
+    public function cadastrarAction()
+    {
+        $this->view->setFile('index');
+
+        $post = $this->request->post();
+
+        if(!empty($post)) {
+            $mandado = array(
+                'descricao' => $post['descricao'],
+                'numero_protocolo' => $post['numero_protocolo'],
+                'id_interessado' => $post['id_interessado'],
+                'id_promotoria' => $post['id_promotoria']
+            );
+
+            $resposta = Mandado::cadastrar($mandado);
+
+            if($resposta->status) {
+                $mandado = $resposta->mandado;
+
+                $this->load('Services\DateConverter');
+
+                $diligencia = array(
+                    'prazo_cumprimento' => $this->dateconverter->toMySqlFormat($post['prazo_cumprimento']),
+                    'id_mandado' => $resposta->mandado->id,
+                    'id_tipo_diligencia' => $post['id_tipo_diligencia']
+                );
+
+                $resposta = Diligencia::cadastrar($diligencia);
+
+                if($resposta->status) {
+                    $this->load('Helpers\Alert', array(
+                        'success',
+                        'Diligência cadastrada!',
+                        "A diligência <strong>$mandado->numero_protocolo</strong> foi cadastrada com sucesso."
+                    ));
+
+                    $evento = array(
+                        'evento_ocorrido' => 'Registro',
+                        'data' => date('Y-m-d H:i:s'),
+                        'id_diligencia' => $resposta->diligencia->id,
+                        'id_autor' => $this->auth->getUserId()
+                    );
+
+                   Evento::cadastrar($evento);
+                } else {
+                    $this->load('Helpers\Alert', array(
+                        'danger',
+                        'Não foi possível completar o cadastro!',
+                        $resposta->errors
+                    ));
+                }
+            } else {
+                $this->load('Helpers\Alert', array(
+                    'danger',
+                    'Não foi possível completar o cadastro!',
+                    $resposta->errors
+                ));
+
+            }
+        }
+    }
 }
