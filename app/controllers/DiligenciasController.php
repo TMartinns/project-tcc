451f06c7b598e7f9175760c52218ce1519ad5d66
Thales Martins <thales.martins35@gmail.com>
2018-08-13 20:25:06 -0300

    Foram adicionados duas novas actions no controller "diligencias", as actions "enviar" e "receber".

--- app/controllers/DiligenciasController.php
+++ app/controllers/DiligenciasController.php
@@ -40,7 +40,7 @@ class DiligenciasController extends \HXPHP\System\Controller
 
         $post = $this->request->post();
 
-        if(!empty($post)) {
+        if (!empty($post)) {
             $mandado = array(
                 'descricao' => $post['descricao'],
                 'numero_protocolo' => $post['numero_protocolo'],
@@ -50,7 +50,7 @@ class DiligenciasController extends \HXPHP\System\Controller
 
             $resposta = Mandado::cadastrar($mandado);
 
-            if($resposta->status) {
+            if ($resposta->status) {
                 $mandado = $resposta->mandado;
 
                 $this->load('Services\DateConverter');
@@ -63,21 +63,23 @@ class DiligenciasController extends \HXPHP\System\Controller
 
                 $resposta = Diligencia::cadastrar($diligencia);
 
-                if($resposta->status) {
+                if ($resposta->status) {
                     $this->load('Helpers\Alert', array(
                         'success',
                         'Diligência cadastrada!',
                         "A diligência <strong>$mandado->numero_protocolo</strong> foi cadastrada com sucesso."
                     ));
 
+                    $eventoRegistro = TipoEvento::find_by_tipo('Registro');
+
                     $evento = array(
-                        'evento_ocorrido' => 'Registro',
                         'data' => date('Y-m-d H:i:s'),
                         'id_diligencia' => $resposta->diligencia->id,
-                        'id_autor' => $this->auth->getUserId()
+                        'id_autor' => $this->auth->getUserId(),
+                        'id_tipo_evento' => $eventoRegistro->id
                     );
 
-                   Evento::cadastrar($evento);
+                    Evento::cadastrar($evento);
                 } else {
                     $this->load('Helpers\Alert', array(
                         'danger',
@@ -92,6 +94,175 @@ class DiligenciasController extends \HXPHP\System\Controller
                     $resposta->errors
                 ));
 
+            }
+        }
+    }
+
+    public function enviarAction()
+    {
+        $this->view->setFile('index');
+
+        $this->request->setCustomFilters(array(
+            'diligencias' => array(
+                'filter' => FILTER_SANITIZE_NUMBER_INT,
+                'flags' => FILTER_FORCE_ARRAY
+            )
+        ));
+
+        $post = $this->request->post();
+
+        if (!empty($post)) {
+            $id_destinatario = (empty($post['idUsuarioEspecifico'])) ? null : $post['idUsuarioEspecifico'];
+
+            if (is_null($id_destinatario) && $post['destinatario'] == 'O') {
+                $pessoas = Pessoa::find_by_sql(
+                    "SELECT * 
+                    FROM pessoas 
+                    INNER JOIN usuarios 
+                    ON pessoas.id = usuarios.id_pessoa 
+                    WHERE usuarios.funcao = 'O' AND usuarios.is_ativo = 1
+                    ORDER BY pessoas.nome"
+                );
+
+                $remessas = Remessa::find_by_sql("SELECT * FROM remessas ORDER BY data DESC");
+
+                $id_destinatario = $pessoas[0]->id;
+
+                if (!empty($remessas)) {
+                    foreach ($remessas as $remessa) {
+                        foreach ($pessoas as $key => $pessoa) {
+                            if ($remessa->id_destinatario == $pessoa->id) {
+                                if (isset($pessoas[$key + 1])) {
+                                    $id_destinatario = $pessoas[$key + 1]->id;
+                                }
+
+                                break 2;
+                            }
+                        }
+                    }
+                }
+            }
+
+            $remessa = array(
+                'data' => date('Y-m-d H:i:s'),
+                'id_remetente' => $this->auth->getUserId(),
+                'id_destinatario' => $id_destinatario
+            );
+
+            $resposta = Remessa::cadastrar($remessa);
+
+            if ($resposta->status) {
+                $diligencias = $post['diligencias'];
+
+                if (!empty($diligencias)) {
+                    $this->load('Storage\Session');
+
+                    $ultimasDiligencias = md5(implode($diligencias));
+
+                    if (!$this->session->exists('ultimasDiligenciasEnviadas') || $this->session->get('ultimasDiligenciasEnviadas') != $ultimasDiligencias) {
+                        $this->session->set('ultimasDiligenciasEnviadas', $ultimasDiligencias);
+
+                        foreach ($diligencias as $diligencia) {
+                            AuxDiligenciasRemessas::cadastrar(array(
+                                'id_remessa' => $resposta->remessa->id,
+                                'id_diligencia' => $diligencia
+                            ));
+
+                            $eventoEnvio = TipoEvento::find_by_tipo('Envio');
+
+                            $evento = array(
+                                'data' => date('Y-m-d H:i:s'),
+                                'id_diligencia' => $diligencia,
+                                'id_autor' => $this->auth->getUserId(),
+                                'id_tipo_evento' => $eventoEnvio->id
+                            );
+
+                            Evento::cadastrar($evento);
+                        }
+
+                        $remessa = $resposta->remessa;
+
+                        $this->load('Helpers\Alert', array(
+                            'success',
+                            'Envio efetuado com sucesso!',
+                            "Todas as diligências selecionadas foram enviadas na remessa <strong>$remessa->id</strong>."
+                        ));
+                    } else {
+                        $resposta->remessa->delete();
+                        echo "Refresh";
+                    }
+                } else {
+                    $resposta->remessa->delete();
+
+                    $this->load('Helpers\Alert', array(
+                        'danger',
+                        'Não foi possível completar o envio!',
+                        'Você precisa selecionar pelo menos uma diligência.'
+                    ));
+                }
+            } else {
+                $this->load('Helpers\Alert', array(
+                    'danger',
+                    'Não foi possível completar o envio!',
+                    $resposta->errors
+                ));
+            }
+        }
+    }
+
+    public function receberAction()
+    {
+        $this->view->setFile('index');
+
+        $this->request->setCustomFilters(array(
+            'remessas' => array(
+                'filter' => FILTER_SANITIZE_NUMBER_INT,
+                'flags' => FILTER_FORCE_ARRAY
+            )
+        ));
+
+        $post = $this->request->post();
+
+        if (!empty($post)) {
+            $remessas = $post['remessas'];
+
+            if (!is_null($remessas)) {
+                $this->load('Storage\Session');
+
+                $ultimasRemessas = md5(implode($remessas));
+
+                if (!$this->session->exists('ultimasRemessasRecebidas') || $this->session->get('ultimasRemessasRecebidas') != $ultimasRemessas) {
+                    $this->session->set('ultimasRemessasRecebidas', $ultimasRemessas);
+
+                    foreach ($remessas as $remessa) {
+                        $diligencias = AuxDiligenciasRemessas::find_all_by_id_remessa($remessa);
+
+                        $eventoRecebimento = TipoEvento::find_by_tipo('Recebimento');
+
+                        foreach ($diligencias as $diligencia) {
+                            Evento::cadastrar(array(
+                                'data' => date('Y-m-d H:i:s'),
+                                'id_diligencia' => $diligencia->id_diligencia,
+                                'id_autor' => $this->auth->getUserId(),
+                                'id_tipo_evento' => $eventoRecebimento->id
+                            ));
+                        }
+
+                        Remessa::receber($remessa);
+                    }
+
+                    $this->load('Helpers\Alert', array(
+                        'success',
+                        'Recebimento efetuado!',
+                        'Todas as remessas selecionadas foram recebidas com sucesso!'
+                    ));
+                }
+            } else {
+                $this->load('Helpers\Alert', array(
+                    'danger',
+                    'Não foi possível completar o recebimento!',
+                    'Você precisa selecionar pelo menos uma remessa.'
+                ));
             }
         }
     }
