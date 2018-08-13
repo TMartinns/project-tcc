10fdce092b5f03e6b61b48564d18e2f27dd8478b
Thales Martins <thales.martins35@gmail.com>
2018-08-13 20:23:50 -0300

    Foram adicionados na model "remessa" duas novas funções, "cadastrar" e "receber".

--- app/models/Remessa.php
+++ app/models/Remessa.php
@@ -3,4 +3,44 @@
 class Remessa extends \HXPHP\System\Model
 {
     static $table_name = 'remessas';
+
+    static $validates_presence_of = array(
+        array(
+            'id_destinatario',
+            'message' => 'O destinatário é um campo obrigatório.'
+        )
+    );
+
+    public static function cadastrar(array $post)
+    {
+        $resposta = new \stdClass;
+        $resposta->remessa = null;
+        $resposta->status = false;
+        $resposta->errors = array();
+
+        $remessa = self::create($post);
+
+        if($remessa->is_valid()) {
+            $resposta->remessa = $remessa;
+            $resposta->status = true;
+            return $resposta;
+        }
+
+        $errors = $remessa->errors->get_raw_errors();
+
+        foreach ($errors as $key => $message) {
+            array_push($resposta->errors, $message[0]);
+        }
+
+        return $resposta;
+    }
+
+    public static function receber($id)
+    {
+        $remessa = self::find_by_id($id);
+
+        $remessa->status = 'R';
+
+        $remessa->save(false);
+    }
 }
