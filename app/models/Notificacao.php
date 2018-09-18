036dfc40a53e61a6310a497c50f18bf2477502f7
Thales Martins <thales.martins35@gmail.com>
2018-09-18 13:00:42 -0300

    Adicionadas duas novas funções a model "notificacao", tais funções permitem registrar uma nova notificação e marcar uma determinada notificação como vista.

--- app/models/Notificacao.php
+++ app/models/Notificacao.php
@@ -3,4 +3,17 @@
 class Notificacao extends \HXPHP\System\Model
 {
     static $table_name = 'notificacoes';
+
+    public static function cadastrar(array $atributos)
+    {
+        self::create($atributos);
+    }
+
+    public static function visto($id)
+    {
+        $notificacao = self::find_by_id($id);
+        $notificacao->visto = 1;
+
+        $notificacao->save(false);
+    }
 }
