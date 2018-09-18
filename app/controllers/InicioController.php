df25a3cf8a66a8111a87de0f71d4543aee13211e
Thales Martins <thales.martins35@gmail.com>
2018-09-18 20:29:22 -0300

    Adicionado algoritmo para verificar o prazo das diligências sempre que o usuário do tipo "oficial de promotoria" acessa o sistema.

--- app/controllers/InicioController.php
+++ app/controllers/InicioController.php
@@ -15,6 +15,8 @@ class InicioController extends \HXPHP\System\Controller
 
         $this->auth->redirectCheck(false);
 
+        $this->auth->roleCheck(array('C', 'O'));
+
         $this->load('Helpers\Menu',
             $this->request,
             $this->configs,
@@ -22,4 +24,57 @@ class InicioController extends \HXPHP\System\Controller
         );
     }
 
+    public function indexAction()
+    {
+        if($this->auth->getUserRole() == 'O') {
+            $this->verificarPrazo();
+        }
+    }
+
+    private function verificarPrazo()
+    {
+        $diligencias = Diligencia::all(array(
+            'conditions' => array('status = ?', 'A')
+        ));
+
+        foreach ($diligencias as $diligencia) {
+            $tipoDiligenciaUrgente = TipoDiligencia::find_by_tipo('Urgente');
+
+            if($diligencia->id_tipo_diligencia != $tipoDiligenciaUrgente->id) {
+                $evento = Evento::find_by_id_diligencia($diligencia->id, array(
+                    'order' => 'data desc'
+                ));
+
+                if ($evento->id_autor == $this->auth->getUserId()) {
+                    if(empty(Notificacao::find_by_id_diligencia($diligencia->id))) {
+
+                        $dataAtual = new DateTime(date('Y-m-d'));
+                        $prazoCumprimento = new DateTime($diligencia->prazo_cumprimento->format('Y-m-d'));
+
+                        $intervalo = $dataAtual->diff($prazoCumprimento);
+
+                        if ($intervalo->d == 1) {
+                            $notificacao = array(
+                                'id_diligencia' => $diligencia->id,
+                                'id_destinatario' => $this->auth->getUserId(),
+                                'mensagem' => 'O prazo de uma diligência está se esgotando, clique para visualizá-la.',
+                                'data' => date('Y-m-d H:i:s')
+                            );
+
+                            Notificacao::cadastrar($notificacao);
+                        } else if ($intervalo->invert == 1) {
+                            $notificacao = array(
+                                'id_diligencia' => $diligencia->id,
+                                'id_destinatario' => $this->auth->getUserId(),
+                                'mensagem' => 'Uma diligência não foi cumprida dentro do prazo, clique para visualizá-la.',
+                                'data' => date('Y-m-d H:i:s')
+                            );
+
+                            Notificacao::cadastrar($notificacao);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
