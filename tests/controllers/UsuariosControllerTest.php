78b6ebda86555123eca80d017c85a718c12ab882
Thales Martins <thales.martins35@gmail.com>
2018-10-01 12:46:43 -0300

    Removido testes que serão impossíveis de fazer no momento.

--- tests/controllers/UsuariosControllerTest.php
+++ tests/controllers/UsuariosControllerTest.php
@@ -5,11 +5,6 @@ use PHPUnit\Framework\TestCase;
 class UsuariosControllerTest extends TestCase
 {
 
-    public function testGetUsuariosAtivosAction()
-    {
-
-    }
-
     public function testGetUsuarioFuncaoAction()
     {
 
